e29dd018649fec41930c1d4c33672d7692c297d3
gouthambijja <103869548+gouthambijja@users.noreply.github.com>
2023-04-11 12:18:05 +0530

    Update robot.py

--- robot.py
+++ robot.py
@@ -7,6 +7,17 @@ import network
 import socket
 import _thread
 from machine import Pin
+from machine import I2C
+from lcd_api import LcdApi
+from pico_i2c_lcd import I2cLcd
+
+#lcd setup
+I2C_ADDR = 39
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+
+i2c = I2C(1,sda = machine.Pin(14),scl = machine.Pin(15),freq = 400000)
+lcd = I2cLcd(i2c,I2C_ADDR, I2C_NUM_ROWS,I2C_NUM_COLS)
 
 
 #pin setup
@@ -16,11 +27,12 @@ m1acw = Pin(4,Pin.OUT)
 m2cw = Pin(5,Pin.OUT)
 m2acw = Pin(6,Pin.OUT)
 gcutter = Pin(7,Pin.OUT)
-plougher = Pin(8,Pin.OUT)
+plougherDown = Pin(8,Pin.OUT)
+plougherUp = Pin(16,Pin.OUT)
 ssower = Pin(9,Pin.OUT)
 trigger = Pin(10,Pin.OUT)
 echo = Pin(11,Pin.OUT)
-gcutter.value(1);
+gcutter.value(0)
 ipAdd=''
 
 ssid = 'Goutham'
@@ -63,7 +75,10 @@ else:
     status = wlan.ifconfig()
     print( 'ip = ' + status[0] )
     ipAdd = status[0]
-    
+    lcd.clear()
+    lcd.move_to(0,0)
+    lcd.putstr(status[0])
+
     
 # Open socket
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
@@ -77,19 +92,20 @@ automatic_flag = 0
 
 
 
-
 def ultra():
+   print("hello")
    if automatic_flag:
        trigger.low()
        utime.sleep_us(2)
        trigger.high()
        utime.sleep_us(5)
        trigger.low()
+       print('ehlo')
        while echo.value() == 0:
            signaloff = utime.ticks_us()
        while echo.value() == 1:
            signalon = utime.ticks_us()
-           
+       print('eho')
        timepassed = signalon - signaloff
        distance = (timepassed * 0.0343) / 2
        print(distance)
@@ -98,26 +114,37 @@ def ultra():
 def move():
     print("helo")
     rotation=0
+    direction_change = 0
+    print(automatic_flag)
     while(automatic_flag):
-        time.sleep(1);
+        if(direction_change == 1):
+            time.sleep(8.2)
+        else:
+            time.sleep(1)
         distance = ultra()
+        print(distance)
         if(distance > 50):
+            direction_change = 0
+            print('forward')
             m1acw.value(0)
             m1cw.value(1)
             m2acw.value(0)
             m2cw.value(1)
         else:
+            direction_change = 1
             if(rotation):
+                print('left')
+                m1acw.value(0)
+                m1cw.value(0)
+                m2cw.value(1)
+                m2acw.value(0)
+                rotation = 0
+            else:
+                print('right')
                 m1acw.value(0)
                 m1cw.value(1)
                 m2cw.value(0)
-                m2acw.value(1)
-                rotation=0
-            else:
-                m1cw.value(0)
                 m2acw.value(0)
-                m1acw.value(1)
-                m2cw.value(1)
                 rotation=1
 
 # Listen for connections, serve client
@@ -164,9 +191,13 @@ while True:
         elif grasscutter_off == 8:
             gcutter.value(1)
         elif plougher_on == 8:
-            plougher.value(1)
+            plougherDown.value(1)
+            time.sleep(0.5)
+            plougherDown.value(0)
         elif plougher_off == 8:
-            plougher.value(0)
+            plougherUp.value(1)
+            time.sleep(0.5)
+            plougherUp.value(0)
         elif seedsower_on == 8:
             ssower.value(1)
         elif seedsower_off == 8:
@@ -186,11 +217,11 @@ while True:
             m1acw.value(0)
             m1cw.value(1)
             m2cw.value(0)
-            m2acw.value(1)
+            m2acw.value(0)
         elif left == 8:
             m1cw.value(0)
             m2acw.value(0)
-            m1acw.value(1)
+            m1acw.value(0)
             m2cw.value(1)
         elif stop == 8:
             m1cw.value(0)
@@ -218,3 +249,4 @@ while True:
     except OSError as e:
         cl.close()
         print('connection closed')
+
