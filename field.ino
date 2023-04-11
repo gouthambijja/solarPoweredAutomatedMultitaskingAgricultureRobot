dc08bcad79dd8f3b17cba6c0a421f4c088128cc1
gouthambijja <103869548+gouthambijja@users.noreply.github.com>
2023-04-11 12:18:33 +0530

    Update field.ino

--- field.ino
+++ field.ino
@@ -1,37 +1,30 @@
-//
-// Copyright 2015 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-// FirebaseDemo_ESP8266 is a sample that demo the different functions
-// of the FirebaseArduino API.
 
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+#include "DHT.h"
+#define DHTTYPE DHT11
 
+#define DHTPIN D1     
+#define FLAMEPIN D2
+#define MOTORPUMP D8
 // Set these to run example.
 #define FIREBASE_HOST "spamar-71800-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "m18p4HSkzsZ6D3J8YDip5G2EFuryXjGG28Sg7IiN"
 #define WIFI_SSID "Goutham"
 #define WIFI_PASSWORD "KakaRot@8897139849"
 
+//dht pin
+DHT dht(DHTPIN, DHTTYPE);
+
 const int sensor_pin = A0;
 int n = 0;
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(FLAMEPIN,INPUT);
+  pinMode(MOTORPUMP,OUTPUT);
+  dht.begin();
+  // dht.begin();
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -45,37 +38,50 @@ void setup() {
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-
-  //pins 
-  
+  digitalWrite(MOTORPUMP,HIGH);
+  //pins
 }
 
 
 
 void loop() {
+  delay(2000);
   float moisture_percentage;
-  moisture_percentage = (100.00 - ((analogRead(sensor_pin)/1023.00)*100));
+  moisture_percentage = (100.00 - ((analogRead(sensor_pin) / 1023.00) * 100));
   // set value
-  Firebase.setFloat("soilmoisture", moisture_percentage);
+  if(moisture_percentage <= 25){
+    digitalWrite(MOTORPUMP,LOW);
+    Serial.println("motor Pump on");
+  }
+  else{
+    digitalWrite(MOTORPUMP,HIGH);
+  }
+  float humidity_percentage = dht.readHumidity();
+
+  bool flame = !digitalRead(FLAMEPIN);
+  Serial.println(flame);
+  Firebase.setFloat("kakarotSoilMoisture", moisture_percentage);
+  Firebase.setFloat("kakarotHumidity", humidity_percentage);
+  Firebase.setBool("kakarotFlame", flame);
+  
   // handle error
   if (Firebase.failed()) {
-      Serial.print("setting /number failed:");
-      Serial.println(Firebase.error());  
-      return;
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+    return;
   }
-  delay(1000);
-  
+
   // // update value
   // Firebase.setFloat("number", 43.0);
   // // handle error
   // if (Firebase.failed()) {
   //     Serial.print("setting /number failed:");
-  //     Serial.println(Firebase.error());  
+  //     Serial.println(Firebase.error());
   //     return;
   // }
   // delay(1000);
 
-  // // get value 
+  // // get value
   // Serial.print("number: ");
   // Serial.println(Firebase.getFloat("soilmoisture"));
   // delay(1000);
@@ -89,17 +95,17 @@ void loop() {
   // // handle error
   // if (Firebase.failed()) {
   //     Serial.print("setting /message failed:");
-  //     Serial.println(Firebase.error());  
+  //     Serial.println(Firebase.error());
   //     return;
   // }
   // delay(1000);
-  
+
   // // set bool value
   // Firebase.setBool("truth", false);
   // // handle error
   // if (Firebase.failed()) {
   //     Serial.print("setting /truth failed:");
-  //     Serial.println(Firebase.error());  
+  //     Serial.println(Firebase.error());
   //     return;
   // }
   // delay(1000);
@@ -109,7 +115,7 @@ void loop() {
   // // handle error
   // if (Firebase.failed()) {
   //     Serial.print("pushing /logs failed:");
-  //     Serial.println(Firebase.error());  
+  //     Serial.println(Firebase.error());
   //     return;
   // }
   // Serial.print("pushed: /logs/");
